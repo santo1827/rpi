b15b44f823a33236d17547e3f86263f665d51c75
santo1827 <136288759+santo1827@users.noreply.github.com>
2023-10-02 11:39:55 +1000

    Message

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/weather', async (req, res) => {
     }
 });
 
-console.log('Hello');
+console.log('This is awesome');
 
 app.listen(5000, () => console.log('Server listening on port 5000'));
